f8a0a853f900066a69666af7005ab0b81fb259e1
Luiz Arthur Cabral <122813785+LuizArthurCabral@users.noreply.github.com>
2023-05-19 20:54:32 -0300

    Add files via upload

---
+++ aula4questao02.py
@@ -0,0 +1,9 @@
+value1 = int(input("Digite um valor: "))
+if value1 > 10 and value1 <= 50:
+    print("Entre 10 e 50")
+
+elif value1 < 10:
+    print("O número vem antes de 10")
+else:
+    print("O número é maior que 50")
+    
